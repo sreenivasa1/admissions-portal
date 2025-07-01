92b46b4da48b253d2161a5a63a9bb2ec65a50c02
sreenivasa1 <crp.srinivas@gmail.com>
2025-07-01 13:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxwb-dzaEeeIrRdTzNP_b_J-MjoUCnamPQ0dhCgXePsZIrHs0NYMuARiMre-5MC2Eb7/exec';
-const form = document.forms['submit-to-google-sheet'];
-const msg = document.getElementById("msg");
+document.addEventListener("DOMContentLoaded", () => {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxwb-dzaEeeIrRdTzNP_b_J-MjoUCnamPQ0dhCgXePsZIrHs0NYMuARiMre-5MC2Eb7/exec';
+  const form = document.forms['submit-to-google-sheet'];
+  const msg = document.getElementById("msg");
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      msg.innerHTML = "We will notify you soon!";
-      form.reset();
-    })
-    .catch(error => {
-      msg.innerHTML = "Something went wrong. Please try again.";
+  if (form) {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          msg.innerHTML = "We will notify you soon!";
+          form.reset();
+        })
+        .catch(error => {
+          msg.innerHTML = "Something went wrong. Please try again.";
+        });
     });
+  }
 });
